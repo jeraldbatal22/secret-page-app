5ee763d9d5fc9fc7b92451faed2e23101c19badd
Jerald Batal <jebatal@highlysucceed.com>
2025-11-22 15:55:00 +0800

    Update UI auth form

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 
 import MainLayout from "@/components/layout";
-import { AuthModal } from "@/components/modals/auth-modal";
-import { Button } from "@/components/ui/button";
+import { AuthForm } from "@/components/modals/auth-form";
 import { useAppSelector } from "@/lib/hooks";
-import { useState } from "react";
 
 export default function HomePage() {
-  const [isOpenAuthModal, setIsOpenAuthModal] = useState(false);
   const { isAuthenticated } = useAppSelector((state) => state.auth);
 
   return (
     <MainLayout>
       <div className="flex min-h-screen items-center justify-center  gap-3">
-        {!isAuthenticated && (
-          <>
-            <Button onClick={() => setIsOpenAuthModal(true)}>
-              Login/Register
-            </Button>
-            <AuthModal
-              isOpen={isOpenAuthModal}
-              onHandleToggleModal={() => setIsOpenAuthModal(false)}
-            />
-          </>
-        )}
+        {!isAuthenticated && <AuthForm />}
       </div>
     </MainLayout>
   );
